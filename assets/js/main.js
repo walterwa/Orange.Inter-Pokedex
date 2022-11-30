8a9318a4a2c38f17fbded1095b335ef51706c026
Walter Andrade <walterwagner.a@gmail.com>
2022-11-30 18:10:20 -0300

    fetch e primeiros codigos no JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('Sucesso!');
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+//arrow functions
+    .then((response) => response.json()) 
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
+
+console.log('sucesso!');
