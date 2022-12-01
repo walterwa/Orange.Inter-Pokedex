62e6044fdbc52d1d0fa58a237b0146c5f6bf6279
Walter Andrade <65465090+walterwa@users.noreply.github.com>
2022-12-01 15:39:12 -0300

    ajustes de poke-api em aberto

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,19 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) =>{
+    fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
     .then((response) => response.json()) 
     .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map((pokemon) => ))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonDetails) =>  {
+        debugger
+        console.log(pokemonDetails)
+    })
     .catch((error) => console.error(error))
 }
